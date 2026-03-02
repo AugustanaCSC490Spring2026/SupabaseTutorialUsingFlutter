e31a3f2a9c7a541943ee8a3d53b2475252746f06
Emily Bautista <emilybautista23@augustana.edu>
2026-03-01 23:01:22 -0600

    Connected supabase to the project

--- supabase_tutorial/lib/main.dart
+++ supabase_tutorial/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  // supabase setup
+  await Supabase.initialize (
+    url: 'https://vpofkwpogwmqmvsiicsx.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwb2Zrd3BvZ3dtcW12c2lpY3N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMjgwMzMsImV4cCI6MjA4NzcwNDAzM30.E0xRmHTENslG23MqPAtHcjCPZxku8malAWgrmUeA72o',
+  );
   runApp(const MainApp());
 }
 
@@ -11,11 +16,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      
     );
   }
 }
